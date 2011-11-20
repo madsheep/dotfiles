586732e53ef3cbd1edd47b6ef1b674c1ca78b910
Adam Nowak <adam.nowak@netguru.pl>
2011-11-20 15:19:04 +0100

    Added script for clean install

---
+++ clean_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf ~/.dotfiles ~/.bash ~/.bash_profile ~/.bashrc ~/.bin ~/.gemrc ~/.gitignore ~/.gvimrc ~/.irbrc ~/.localrc ~/.railsrc ~/.vim ~/.vimrc ~/.zlogin ~/.zsh ~/.zshrc
+
+./install.sh
