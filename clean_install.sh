f4df39cfbea49a20c764798c812997e869b43401
Adam Nowak <adam.nowak@netguru.pl>
2012-04-21 19:48:32 +0200

    bashrc_help fix #2

--- clean_install.sh
+++ clean_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf ~/.dotfiles ~/.bash ~/.bash_profile ~/.bashrc ~/.bin ~/.gemrc ~/.gitignore ~/.gvimrc ~/.irbrc ~/.localrc ~/.railsrc ~/.vim ~/.vimrc ~/.zlogin ~/.zsh ~/.zshrc
+rm -rf ~/.dotfiles ~/.bash ~/.bash_profile ~/.bash_help ~/.bashrc ~/.bin ~/.gemrc ~/.gitignore ~/.gvimrc ~/.irbrc ~/.localrc ~/.railsrc ~/.vim ~/.vimrc ~/.zlogin ~/.zsh ~/.zshrc
 
 ./install.sh
--- install.sh
+++ install.sh
@@ -6,27 +6,23 @@ if [[ $mydir = . ]]; then
   mydir=`pwd`
 fi
 
-# Updating submodules
-
-# git submodule update --init
-# git submodule foreach git clean -fdx
-# git submodule foreach git pull origin master
-
 ryanb_dotfiles_dir=~/.dotfiles
 iain_osx_settings_dir=$mydir/iain-osx_settings
 twerth_dotfiles_dir=$mydir/twerth-dotfiles
 
-
 # Cloning ryanb/dotfiles
 
 if [ -d $ryanb_dotfiles_dir ]; then
-	echo "Directory exists"
-	cd $ryanb_dotfiles_dir
-	git pull
-else 
-	echo "Directory does not exists"
-	git clone git://github.com/ryanb/dotfiles ~/.dotfiles
+  echo "Directory exists"
+  cd $ryanb_dotfiles_dir
+  git pull
+else
+  echo "Directory does not exists"
+  git clone git://github.com/ryanb/dotfiles ~/.dotfiles
 fi
+
+echo "Copying iTerm2 settings to ~/.dotfiles/iterm2..."
+cp -r $mydir/iterm2 $ryanb_dotfiles_dir
 
 echo "Creating new gitconfig.erb file for dotfiles..."
 cp $mydir/my_gitconfig.erb $ryanb_dotfiles_dir/gitconfig.erb
@@ -34,12 +30,15 @@ cp $mydir/my_gitconfig.erb $ryanb_dotfiles_dir/gitconfig.erb
 echo "Copying irbrc file from osx_settings to dotfiles..."
 cp $iain_osx_settings_dir/.irbrc $ryanb_dotfiles_dir/irbrc
 
+echo "Copying .bashrc_help from twerth/dotfiles to ~/.bashrc_help"
+cp $twerth_dotfiles_dir/etc/bashrc_help ~/.bashrc_help
+
 echo "Copying bashrc file settings from twerth/dotfiles to localrc..."
 cat $twerth_dotfiles_dir/etc/bashrc | grep -Ev "~/cl" > $ryanb_dotfiles_dir/localrc
 cat $mydir/localrc  >> $ryanb_dotfiles_dir/localrc
 
+# Check Ruby
 
-# Check Ruby 
 if echo $rvm_ruby_version | grep -q '1.9'; then
   export RUBYOPT='-r openssl'
 else
@@ -49,9 +48,9 @@ fi
 cd $ryanb_dotfiles_dir
 rake install
 
-echo 'open -a "Sublime Text 2" "$@"' > $mydir/slime
-chmod +x $mydir/slime
-mv $mydir/slime /usr/local/bin/slime
+echo 'open -a "Sublime Text 2" "$@"' > $mydir/sub
+chmod +x $mydir/sub
+mv $mydir/sub /usr/local/bin/sub
 
 echo ""
 echo "Done!"
